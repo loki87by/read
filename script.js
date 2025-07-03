6d6defecf1a31f25048fbc5c7df42aaa23a0ae47
loki87by <loki87.666@gmail.com>
2025-07-03 17:03:36 +0300

    try

---
+++ script.js
@@ -0,0 +1,12 @@
+const url = 'https://knizhnik.org/garri-garrison/krest-i-korol/22'
+fetch(url, {method: 'GET'}).then((res) => 
+console.log(res))
+
+
+const read = document.querySelector('.read')
+const ps = read.querySelectorAll('p')
+const text = Array.from(ps).map(i=>i.textContent).join('\n')
+/* Array.from(ps).forEach(i => {
+    console.log(i.parentNode)
+    nodeName: "H2"
+}) */
